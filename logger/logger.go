9b7ea81538209238565f59147518de3e8dfbe770
Benoit KUGLER <x.ben.x@free.fr>
2021-12-31 09:46:52 +0100

    [logger] documentation

--- logger/logger.go
+++ logger/logger.go
@@ -1,3 +1,5 @@
+// Package logger provides two log.Logger emitting progress status and warning
+// information.
 package logger
 
 import (
@@ -6,8 +8,11 @@ import (
 )
 
 // ProgressLogger logs the main steps of the HTLM rendering.
+// It is purely informative and may be turned off safely.
 var ProgressLogger = log.New(os.Stdout, "webrender.progress: ", log.LstdFlags)
 
 // WarningLogger emits a warning for each non fatal error, like unsupported CSS
-// properties, font loading errors or URL resolutions
+// properties, font loading or URL resolutions errors.
+// It can be turned off safely, but it is a good source of information if the
+// rendering seems wrong.
 var WarningLogger = log.New(os.Stdout, "webrender.warning: ", log.Lmsgprefix)
