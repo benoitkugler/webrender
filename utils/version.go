4577ce2d2ed6e45479d8f198c5c8967601df8b82
Benoit KUGLER <benoit.kugler@gmail.com>
2024-11-19 13:09:14 +0100

    [utils] bump upstream version

--- utils/version.go
+++ utils/version.go
@@ -5,10 +5,10 @@ import (
 )
 
 const (
-	Version = "0.59"
+	Version = "0.62"
 )
 
 // Used for "User-Agent" in HTTP
 var VersionString = fmt.Sprintf("Go-WebRender %s", Version)
 
-// commit of the Python reference implementation 2a9a952c356b856ad1f80ddb6d0c93b36ccac46c
+// commit of the Python reference implementation d5d7ce369aef035712cf73446f9085a32105846f
